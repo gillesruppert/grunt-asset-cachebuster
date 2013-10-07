9844424d51380c54d6dc8a344d740ab1f0727f49
Gilles Ruppert <gilles@madeofbytes.com>
2013-10-07 14:20:27 +0200

    initial commit

---
+++ tasks/asset_cachebuster.js
@@ -0,0 +1,83 @@
+/*
+ * grunt-asset-cachebuster
+ * https://github.com/gillesruppert/grunt-asset-cachebuster
+ *
+ * Copyright (c) 2013 Gilles Ruppert
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+function isCss(filepath) {
+  return (/\.css$/).test(filepath);
+}
+
+function isHtml(filepath, extension) {
+  var htmlTest = new RegExp('\\.' + extension + '$', 'gi');
+  return htmlTest.test(filepath);
+}
+
+function cacheBustCss(css, buster) {
+  var img = /url\(['"]?(?!data:)([^)'"?]+)['"]?(?:\?v=[0-9]+)*\)/gi;
+  return css.replace(img, 'url($1?v=' + buster + ')');
+}
+
+function cacheBustHtml(html, buster) {
+  var css = /href="(.+\.css)"/gi;
+  html = html.replace(css, 'href="$1?v=' + buster + '"');
+
+  var js = /src="(.+\.js)"/gi;
+  html = html.replace(js, 'src="$1?v=' + buster + '"');
+
+  var images = /src="(.+\.)(png|gif|jpg|jpeg)"/gi;
+  html = html.replace(images, 'src="$1$2?v=' + buster + '"');
+  return html;
+}
+
+
+module.exports = function(grunt) {
+
+  function cacheBust(src, files, options) {
+    if (isCss(files.dest)) {
+      grunt.file.write(files.dest, cacheBustCss(src, options.buster));
+    }
+    else if (isHtml(files.dest, options.htmlExtension)) {
+      grunt.file.write(files.dest, cacheBustHtml(src, options.buster));
+    } else {
+      grunt.file.write(files.dest, cacheBustHtml(src, options.buster));
+    }
+    grunt.log.writeln('File "' + files.dest + '" created.');
+  }
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('asset_cachebuster', 'Cachebust images, scripts and other assets in your HTML & CSS files.', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      buster: '123456',
+      htmlExtension: 'html'
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(files) {
+      var src = files.src.filter(function(filepath) {
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function (filepath) { return grunt.file.read(filepath); })
+      .join(grunt.util.normalizelf(''));
+
+      try {
+        cacheBust(src, files, options);
+      } catch (e) {
+        grunt.log.error('ERROR:', e.message, e);
+        grunt.fail.warn('Failed to write file: ' + files.dest);
+      }
+    });
+
+  });
+};
---
+++ test/asset_cachebuster_test.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var grunt = require('grunt');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports.asset_cachebuster = {
+  setUp: function(done) {
+    // setup here if necessary
+    done();
+  },
+  default_options_css: function(test) {
+    test.expect(1);
+
+    var actual = grunt.file.read('tmp/default_options.css');
+    var expected = grunt.file.read('test/expected/default_options.css');
+    test.equal(actual, expected, 'CSS should have versioned images');
+
+    test.done();
+  },
+  default_options_html: function(test) {
+    test.expect(1);
+
+    var actual = grunt.file.read('tmp/default_options.html');
+    var expected = grunt.file.read('test/expected/default_options.html');
+    test.equal(actual, expected, 'HTML should have versioned assets');
+
+    test.done();
+  },
+};
